41724c8e964c72c80ebec1ac12595561d79f6355
Game-Punch <petrosyanartur064@gmail.com>
2024-03-03 02:09:30 +0400

    .

---
+++ front/src/pages/Logistic.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Wrapper from "../component/Wrapper";
+import Header from "../component/Header";
+import Footer from "../component/Footer";
+
+
+function Logistic(){
+    return(
+        <Wrapper>
+            
+        </Wrapper>
+    )
+}
+
+export default Logistic
