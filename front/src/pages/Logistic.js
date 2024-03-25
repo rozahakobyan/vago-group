40e71a0f33eb0ca17208dcde8b72bbfb03ed768d
Arthur <petrosyanartur064@GMAIL.com>
2024-03-25 17:08:17 +0400

    .

--- front/src/pages/Logistic.js
+++ front/src/pages/Logistic.js
@@ -2,13 +2,15 @@ import React from "react";
 import Price from "../component/logistic/Price";
 import Wrapper from "../component/Wrapper";
 import Logistic_banner from "../component/logistic/logistic-banner";
+import Logistic_contacts from "../component/logistic/logistic-contacts";
 
 
 function Logistic(){
     return(
         <Wrapper>
             <Logistic_banner/>
-            <Price/>            
+            <Price/>    
+            <Logistic_contacts/>        
         </Wrapper>
     )
 }
