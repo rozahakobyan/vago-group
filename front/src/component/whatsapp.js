b54cdf149bfed76c273d81ccaaae46e608ddbf15
rozahakobyan <rhakobyan290@gmail.com>
2024-04-18 18:06:50 +0400

    scroll up button

--- front/src/component/whatsapp.js
+++ front/src/component/whatsapp.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { ReactComponent as ArrowOpen } from "../assets/icon/arrowOpen.svg";
-
-
+import ScrollToTop from "react-scroll-up";
 
 function Whatsapp() {
     return (
         <div>
             <div className="fixed-button-area">
-                <button className="fixed-block-1"></button>
-                <ArrowOpen className="fixed-block-1" />
+                <ScrollToTop showUnder={160}>
+                    <ArrowOpen className="fixed-block-1" />
+                </ScrollToTop>
             </div>
             <div className="fixed-button-area">
                 <a href="https://wa.me/+37477571551"><div className="fixed-block"></div></a>
