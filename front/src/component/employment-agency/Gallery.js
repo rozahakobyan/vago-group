2d4032f7349418a1bd5f8f54fb0d5da31789f0d9
Game-Punch <petrosyanartur064@gmail.com>
2024-05-01 13:13:33 +0400

    .

--- front/src/component/employment-agency/Gallery.js
+++ front/src/component/employment-agency/Gallery.js
@@ -1,7 +1,25 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { galleriesListRequest } from "../../store/actions/galleries";
+import ReactPaginate from "react-paginate";
+import { API_URL } from "../../Api"
+
 
 function Gallery() {
+    const dispatch = useDispatch();
+
     const [fullscreenImg, setFullscreenImg] = useState("");
+    const [page, setPage] = useState(1);
+
+    const pages = useSelector(state => state.galleries.pages);
+    const galleriesList = useSelector(state => state.galleries.galleriesList);
+    const loading = useSelector(state => state.galleries.loading);
+
+    useEffect(() => {
+        dispatch(galleriesListRequest({ page, limit: 4, pageGallery: "Employment Agency" }))
+    }, [page]);
+
+    console.log(galleriesList)
 
     const openFullscreenImg = useCallback((src) => {
         setFullscreenImg(src);
@@ -19,10 +37,30 @@ function Gallery() {
             <div className="gallery-type">
                 <h3>Photo</h3>
             </div>
-            <div className="gallery-blocks">
-                <img src={"./img/logo.jpg"} onClick={() => openFullscreenImg("./img/logo.jpg")} alt={""}/>
-                <img src={"./img/gortsiq.jpg"} onClick={() => openFullscreenImg("./img/gortsiq.jpg")} alt={""}/>
-                <img src={"./img/logo.jpg"} onClick={() => openFullscreenImg("./img/logo.jpg")} alt={""}/>
+
+            <div className="gallery-blocks" >
+                {galleriesList && galleriesList.map(g => (
+                    <img src={`${API_URL}/${g.src}`} onClick={() => openFullscreenImg(`${API_URL}/${g.src}`)} alt={""} key={g.id}/>
+                ))}
+            </div>
+
+            <div className={"pages-list"}>
+                {pages && pages > 1 ? <ReactPaginate
+                    activeClassName={'items active '}
+                    breakClassName={'items break-me '}
+                    breakLabel={'...'}
+                    containerClassName={'pagination'}
+                    disabledClassName={'disabled-page'}
+                    marginPagesDisplayed={2}
+                    nextClassName={"items next "}
+                    nextLabel={">"}
+                    initialPage={page - 1}
+                    onPageChange={(ev) => setPage(ev.selected + 1)}
+                    pageCount={pages}
+                    pageClassName={'items pagination-page '}
+                    pageRangeDisplayed={2}
+                    previousClassName={"items previous"}
+                    previousLabel={"<"} /> : null}
             </div>
             {fullscreenImg && (
                 <div className="fullscreen-img-overlay" onClick={closeFullscreenImg}>
@@ -37,8 +75,14 @@ function Gallery() {
                 <h3>Video</h3>
             </div>
             <div className="gallery-blocks">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/FwVx8PV3V0M?si=OyHsuVIac0WXC5Sm" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={true}></iframe>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/FwVx8PV3V0M?si=OyHsuVIac0WXC5Sm" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={true}></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/FwVx8PV3V0M?si=OyHsuVIac0WXC5Sm"
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen={true}></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/FwVx8PV3V0M?si=OyHsuVIac0WXC5Sm"
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen={true}></iframe>
             </div>
         </div>
     );
