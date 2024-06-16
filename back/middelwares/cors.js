ed25507cab5667ce060952569881364f7f50d9ee
rozahakobyan <rhakobyan290@gmail.com>
2024-06-16 11:46:49 +0400

    finish

--- back/middelwares/cors.js
+++ back/middelwares/cors.js
@@ -1,7 +1,10 @@
 const ALLOW_ORIGINS = [
   'http://localhost:3000',
+  'https://localhost:3000',
   'http://localhost:3001',
+  'https://localhost:3001',
   'http://localhost:3002',
+  'https://localhost:3002',
   "https://vago-group-admin.vercel.app",
   "https://vago-group.vercel.app"
 ];
@@ -9,7 +12,9 @@ const ALLOW_ORIGINS = [
 export default function cors(req, res, next) {
   try {
     const { origin } = req.headers;
-   
+    // res.setHeader('Access-Control-Allow-Origin', "*");
+    // res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    // res.setHeader('Access-Control-Allow-Headers', 'Authorization,Content-Type');
     if (ALLOW_ORIGINS.includes(origin)) {
       res.setHeader('Access-Control-Allow-Origin', origin);
       res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
