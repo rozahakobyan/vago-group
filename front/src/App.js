4cf9ec2fe0854ee563caa780b60ae00f04ec3088
Game-Punch <petrosyanartur@gmail.com>
2024-06-18 17:42:49 +0400

    .

--- front/src/App.js
+++ front/src/App.js
@@ -13,12 +13,12 @@ import VacanciesDetales from './pages/VacanciecDetales';
 function App() {
   return (
     <Routes>
-      <Route path={'/'} element={<Navigate to={"/homeInformation"} />} />
+      <Route path={'/'} element={<Navigate to={"/home"} />} />
       <Route path={'/login'} element={<Login />} />
       <Route path={'/registration'} element={<Registration/>}/>
       <Route path={'/activate-user/:email'} element={<ActivateUser/>}/>
       <Route path={'/forgot-password'} element={<ForgotPassword />}/>
-      <Route path={"/homeInformation"} element={<Home />} />
+      <Route path={"/home"} element={<Home />} />
       <Route path={"/construction"} element={<Construction />} />
       <Route path={'/employment-agency'} element={<EmploymentAgency/>}/>
       <Route path={'/logistic'} element={<Logistic/>}/>
