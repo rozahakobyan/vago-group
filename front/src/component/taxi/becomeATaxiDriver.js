55f64f5d87e907bdc860b22fa38b27386d95cb56
Game-Punch <petrosyanartur@gmail.com>
2024-06-18 17:37:25 +0400

    .

--- front/src/component/taxi/becomeATaxiDriver.js
+++ front/src/component/taxi/becomeATaxiDriver.js
@@ -1,8 +1,36 @@
 import React from "react";
+import translation from "../../assets/data/translation";
+import {Account} from "../../helpers/Account";
+import Button from "../Button";
+import {useDispatch, useSelector} from "react-redux";
+import Image from "../../assets/images/taxi.jpg"
 
-function TaxiDriver () {
-    return(
-        <></>
+function TaxiDriver() {
+    const language = Account.getLanguage();
+    const loading = useSelector(state => state.users.loading)
+
+    return (
+        <div className="becomeTaxiDriver">
+            <div className="FormBoxArea">
+                <form onSubmit={''}>
+                    <input type={'text'} placeholder={translation.offerName[language]}/>
+                    <input type={"text"} placeholder={translation.offerPhone[language]}/>
+                    <Button title={translation.submit[language]} loading={loading}/>
+                </form>
+            </div>
+            <div className="imageBoxArea" style={{
+                width: "320px",
+                height:"400px",
+                backgroundImage:`url(${Image})`,
+                backgroundRepeat:"none",
+                backgroundPosition:"center",
+                margin: "25px",
+                backgroundSize: "cover",
+            }}>
+                
+            </div>
+
+        </div>
     )
 }
 
