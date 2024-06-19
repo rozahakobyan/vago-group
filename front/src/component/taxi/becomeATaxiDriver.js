65968aa78606775d991be228e49535ae4734e041
Game-Punch <petrosyanartur@gmail.com>
2024-06-19 06:37:30 +0400

    .

--- front/src/component/taxi/becomeATaxiDriver.js
+++ front/src/component/taxi/becomeATaxiDriver.js
@@ -1,8 +1,8 @@
 import React from "react";
 import translation from "../../assets/data/translation";
-import {Account} from "../../helpers/Account";
+import { Account } from "../../helpers/Account";
 import Button from "../Button";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Image from "../../assets/images/taxi.jpg"
 
 function TaxiDriver() {
@@ -11,25 +11,31 @@ function TaxiDriver() {
 
     return (
         <div className="becomeTaxiDriver">
-            <div className="FormBoxArea">
-                <form onSubmit={''}>
-                    <input type={'text'} placeholder={translation.offerName[language]}/>
-                    <input type={"text"} placeholder={translation.offerPhone[language]}/>
-                    <Button title={translation.submit[language]} loading={loading}/>
-                </form>
-            </div>
-            <div className="imageBoxArea" style={{
-                width: "320px",
-                height:"400px",
-                backgroundImage:`url(${Image})`,
-                backgroundRepeat:"none",
-                backgroundPosition:"center",
-                margin: "25px",
-                backgroundSize: "cover",
-            }}>
-                
-            </div>
+            <div className="becomeTaxiDriverBox">
+                <div className="imageBoxArea" style={{
+                    width: "320px",
+                    backgroundImage: `url(${Image})`,
+                    backgroundRepeat: "none",
+                    backgroundPosition: "center",
+                    margin: "25px",
+                    backgroundSize: "cover",
+                    textAlign: "center",
+                    height: "400px",
+                    color: "red",
 
+                }}>
+                    <strong>Becom A Taxi Driver</strong>
+                </div>
+                <div className="FormBoxArea">
+                    <form onSubmit={''}>
+                        <label><strong>напишите ваше име и номер и мыпазвоним вам в течении 30 минут</strong></label>
+                        <input type={'text'} placeholder={translation.offerName[language]} />
+                        <input type={"text"} placeholder={translation.offerPhone[language]} />
+                        <Button title={translation.submit[language]} loading={loading} />
+                    </form>
+                </div>
+
+            </div>
         </div>
     )
 }
